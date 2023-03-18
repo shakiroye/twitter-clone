4c761b8b7f5f2f01b3bd2bc27b47d70d260ede35
shakiroye <oyeossiadewoye@gmail.com>
2023-03-18 13:21:04 +0000

    main section

--- app.vue
+++ app.vue
@@ -14,8 +14,8 @@
           </div>
 
           <!-- Main content -->
-          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
-            <h1>asasa</h1>
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view></router-view>
           </main>
 
           <!-- Right sidebar -->
